1ec7274104b8a19d19cfc1760e01719c0a105587
SRIRAM V <118797568+SRIRAM-2304@users.noreply.github.com>
2025-01-01 22:45:10 +0530

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'Sriram-003/sriram',
+  base: 'Sriram-003/portfolio',
 });
